f4150863ee36575ace0ba0c7276ce40ffba29a2d
LUCAS-PC\Lucas <lucaskchang@gmail.com>
2023-03-17 20:49:31 -0700

    spanning tree

--- components/HSRNetworkGravityModels.vue
+++ components/HSRNetworkGravityModels.vue
@@ -11,6 +11,7 @@
     />
     <p class="mt-5 font-bold">Score: {{ Math.round(network_score).toLocaleString("en-US") }}</p>
     <p class="mt-5 font-bold">Stations: {{ selected_csas.length }}</p>
+    <p class="mt-5 font-bold">Network Length: {{ Math.round(length).toLocaleString("en-US") }} miles</p>
 
     <div style="height:500px; width: 100%" class="mt-5">
       <l-map 
@@ -22,11 +23,17 @@
         :options="{ zoomControl: false, scrollWheelZoom: false, dragging: false, doubleClickZoom: false, touchZoom: false, }"
       >
         <l-geo-json :geojson="us_map" :options-style="geoJsonStyles" />
+        <l-polyline
+          v-for="edge in spanning_tree"
+          :key="edge"
+          :lat-lngs="[[CSAs[edge[0]]['Latitude'], CSAs[edge[0]]['Longitude']], [CSAs[edge[1]]['Latitude'], CSAs[edge[1]]['Longitude']]]"
+          :options="{ color: '#a855f7', weight: 2 }" 
+        />
         <l-circle-marker
           v-for="csa in selected_csas"
           :key="csa"
           :lat-lng="[CSAs[csa]['Latitude'], CSAs[csa]['Longitude']]" 
-          :radius="CSAs[csa]['Population']**0.75 / 10000"
+          :radius="CSAs[csa]['Population']**0.75 / 10000 > 1.5 ? CSAs[csa]['Population']**0.75 / 10000 : 1.5"
           :options="{ fillColor: '#a855f7', fillOpacity: 0.5, color: '#a855f7', weight: 1 }"
         >
           <l-popup class="text-center"> {{ csa }} <br /> {{ CSAs[csa]['Population'].toLocaleString("en-US") }} </l-popup>
@@ -49,7 +56,8 @@ import us_map from '../assets/geojson/us_outline.json';
 
 const cities = Object.keys(CSAs)
 const selected_csas = ref([])
-const zoom = ref(4) 
+const zoom = ref(4)
+const length = ref(0)
 
 function getDistanceFromLatLonInKm(lat1 : number, lon1 : number, lat2 : number, lon2 : number) {
   var R = 6371; // Radius of the earth in km
@@ -76,13 +84,44 @@ const geoJsonStyles = {
   weight: 1,
 }
 
+const spanning_tree = computed(function () {
+  if (selected_csas.value.length <= 1) {
+    return []
+  }
+  var forest = []
+  var length_of = 0
+  var vertices = [...selected_csas.value]
+  while (vertices.length > 0) {
+    var vertex = vertices.pop()
+    var vertex_data = CSAs[vertex]
+    var closest_vertex = null
+    var closest_distance = 1000000
+    for (const v of vertices) {
+      var v_data = CSAs[v]
+      var distance = getDistanceFromLatLonInKm(vertex_data['Latitude'], vertex_data['Longitude'], v_data['Latitude'], v_data['Longitude'])
+      if (distance < closest_distance) {
+        closest_distance = distance
+        closest_vertex = v
+      }
+    }
+    if (closest_vertex == null) {
+      continue
+    }
+    length_of += closest_distance
+    forest.push([vertex, closest_vertex])
+  }
+  length.value = length_of / 1.6
+  return forest
+})
+
+
+
 const network_score = computed(function () {
     if (selected_csas.value.length <= 1) {
         return 0
     }
     const done_csas : string[] = []
     var score = 0
-    var total_distance = 0
     // loop each selected CSA
     for (const csa of selected_csas.value) {
         const csa_data = CSAs[csa]
@@ -93,7 +132,6 @@ const network_score = computed(function () {
                 continue
             }
             var distance_between = getDistanceFromLatLonInKm(csa_data['Latitude'], csa_data['Longitude'], csa2_data['Latitude'], csa2_data['Longitude'])
-            total_distance += distance_between
 
             var modifier = 1
             var gravity = csa_data["Population"]**0.8 * csa2_data["Population"]**0.8 / distance_between ** 2
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,8 @@ export default defineNuxtConfig({
     target: 'static',
     modules: [
         '@nuxtjs/tailwindcss',
-    ]
+    ],
+    head: {
+        title: 'Data Visualization',
+    }
 })
